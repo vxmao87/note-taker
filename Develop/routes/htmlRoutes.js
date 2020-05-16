99ca7d8e53457ef2d8c6c519f938476536b5fa69
Vinh Mao <vinhxmao87@gmail.com>
2020-05-16 13:05:27 -0700

    HTML routes updated for deploy to Heroku

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,6 +4,11 @@ const path = require("path");
 // Make the following functions available everywhere in the application
 module.exports = function(app) {
 
+    // GET - returns the home page by default
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
     // GET - returns the notes page
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
